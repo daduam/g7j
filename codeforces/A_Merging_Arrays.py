e2be1b72de9b1d58048a8db9badf9caf355bada3
Joseph Ampadu <jkampadujnr@gmail.com>
2026-03-04 18:49:40 +0000

    add two codeforces

---
+++ codeforces/A_Merging_Arrays.py
@@ -0,0 +1,15 @@
+n, m = map(int, input().split())
+a = list(map(int, input().split()))
+b = list(map(int, input().split()))
+i, j = 0, 0
+ans = []
+while i < n and j < m:
+  if a[i] < b[j]:
+    ans.append(a[i])
+    i += 1
+  else:
+    ans.append(b[j])
+    j += 1
+ans.extend(a[i:])
+ans.extend(b[j:])
+print(' '.join(map(str, ans)))
