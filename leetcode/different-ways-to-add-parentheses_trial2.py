251cbb7cec6c99fa730e268f17aa0f61f5f9435c
Joseph Ampadu <jkampadujnr@gmail.com>
2026-04-16 23:38:31 +0000

    Add solution for Different Ways to Add Parentheses (trial 2)

--- leetcode/different-ways-to-add-parentheses_trial2.py
+++ leetcode/different-ways-to-add-parentheses_trial2.py
@@ -10,16 +10,19 @@ class Solution:
         }
 
         def helper(left, right):
+            if memo[left][right] is not None:
+                return memo[left][right]
             ret = []
             for i in range(left, right+1):
                 if expression[i] in ops:
-                    lnums = memo[left][i-1] if memo[left][i-1] is not None else helper(left, i - 1)
-                    rnums = memo[i+1][right] if memo[i+1][right] is not None else helper(i + 1, right)
+                    lnums = helper(left, i - 1)
+                    rnums = helper(i + 1, right)
                     for a in lnums:
                         for b in rnums:
                             ret.append(ops[expression[i]](a, b))
             if not ret:
                 ret.append(int(expression[left:right+1]))
+            memo[left][right] = ret
             return ret
         
         return helper(0, len(expression)-1)
