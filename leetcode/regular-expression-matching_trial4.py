f1f42bb474077604388a0b4c8b839d0d336f27d7
Joseph Ampadu <jkampadujnr@gmail.com>
2026-04-17 21:35:53 +0000

    Add solution for Regular Expression Matching (trial 4)

--- leetcode/regular-expression-matching_trial4.py
+++ leetcode/regular-expression-matching_trial4.py
@@ -4,23 +4,19 @@ class Solution:
         cache = {}
 
         def helper(si: int, pi: int) -> bool:
-            if pi >= pn:
-                return si >= sn
-            
             if (si, pi) in cache:
                 return cache[(si, pi)]
+            
+            if pi >= pn:
+                return si >= sn
             
             match1 = si < sn and p[pi] in {s[si], '.'}
 
             if pi + 1 < pn and p[pi+1] == '*':
-                if (si, pi+2) not in cache:
-                    cache[(si, pi+2)] = helper(si, pi+2)
-                    
-                return cache[(si, pi+2)] or (match1 and helper(si+1, pi))
+                cache[(si, pi)] = helper(si, pi+2) or (match1 and helper(si+1, pi))
+                return cache[(si, pi)]
             
-            if (si+1, pi+1) not in cache:
-                cache[(si+1, pi+1)] = helper(si+1, pi+1)
-                
-            return match1 and cache[(si+1, pi+1)]
+            cache[(si, pi)] = match1 and helper(si+1, pi+1)
+            return cache[(si, pi)]
 
         return helper(0, 0)
