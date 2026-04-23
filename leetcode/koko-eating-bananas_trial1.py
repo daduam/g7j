f409be59bae3b81eec5acb5236a10fd9ae5f98e6
Joseph Ampadu <jkampadujnr@gmail.com>
2026-04-23 12:30:11 +0000

    Add solution for Koko Eating Bananas (trial 1)

---
+++ leetcode/koko-eating-bananas_trial1.py
@@ -0,0 +1,16 @@
+class Solution:
+    def minEatingSpeed(self, piles: List[int], h: int) -> int:
+        def check(k: int) -> bool:
+            curh = 0
+            for pile in piles:
+                curh += (pile // k) + int(pile % k != 0)
+            return curh <= h
+        
+        lo, hi = 1, max(piles)
+        while lo <= hi:
+            mid = lo + (hi - lo) // 2
+            if check(mid):
+                hi = mid - 1
+            else:
+                lo = mid + 1
+        return lo
